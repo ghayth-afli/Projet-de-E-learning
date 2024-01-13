c56c9a7ddea82f68f6f6dae9b073fa4ee8cc7238
Afli Mohamed Ghayth <40703798+ghayth-afli@users.noreply.github.com>
2024-01-13 20:44:28 +0100

    fix slideshow

--- js/script.js
+++ js/script.js
@@ -8,3 +8,51 @@ btn.addEventListener("click", () => {
     menu.classList.add("nav-open");
   }
 });
+
+var nbslide = 1;
+
+const slide1 = document.getElementById("slide-1");
+const slide2 = document.getElementById("slide-2");
+slide2.classList.add("hide");
+
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+
+next.addEventListener("click", () => {
+  console.log("ok");
+  if (nbslide == 1) {
+    slide2.classList.remove("hide");
+    slide1.classList.add("hide");
+    nbslide = 2;
+  } else {
+    slide1.classList.remove("hide");
+    slide2.classList.add("hide");
+    nbslide = 1;
+  }
+});
+prev.addEventListener("click", () => {
+  if (nbslide == 1) {
+    slide2.classList.remove("hide");
+    slide1.classList.add("hide");
+    nbslide = 2;
+  } else {
+    slide1.classList.remove("hide");
+    slide2.classList.add("hide");
+    nbslide = 1;
+  }
+});
+
+function autoSlide() {
+  if (nbslide == 1) {
+    slide2.classList.remove("hide");
+    slide1.classList.add("hide");
+    nbslide = 2;
+  } else {
+    slide1.classList.remove("hide");
+    slide2.classList.add("hide");
+    nbslide = 1;
+  }
+}
+
+// Set interval for automatic slide transitioning
+autoSlideInterval = setInterval(autoSlide, 5000);
